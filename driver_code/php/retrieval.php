7c6923e9051bf51008b07d9502602ed2b36f332d
vinay kulkarni <kulkarnivinay621@gmail.com>
2022-08-04 09:48:37 +0530

    aug_4

--- driver_code/php/retrieval.php
+++ driver_code/php/retrieval.php
@@ -1,22 +1,24 @@
 <!--retrieval of all students -->
 <?php
-if ($con) {
-    $query = "SELECT * FROM `student`";
-    $res = $con->query($query);
-}
-if ($res) {
-    while ($array = $res->fetch(PDO::FETCH_OBJ)) {
-        echo '
+function query_all($con){
+    if ($con) {
+        $query = "SELECT * FROM `student`";
+        $res = $con->query($query);
+    }
+    if ($res) {
+        while ($array = $res->fetch(PDO::FETCH_OBJ)) {
+            echo '
         <h1>' . $array->usn . '</h1>
         <h1>' . $array->student_email . '</h1>
         <h1>' . $array->student_name . '</h1>
         <h1>' . $array->branch_name . '</h1>
         ';
+        }
+    } else {
+        echo 'Data not found';
     }
-} else {
-    echo 'Data not found';
 }
-?>
+ ?>
 
 
 
@@ -24,20 +26,17 @@ if ($res) {
 
 <!-- retrieveal of some stufs -->
 <?php
-if ($con) {
-    $query = "SELECT `usn`, `student_email`, `student_name`, `branch_name` FROM `student` WHERE `usn`='1BM20CS001'";
-    $res = $con->query($query);
-}
-if ($res) {
-    while ($array = $res->fetch(PDO::FETCH_OBJ)) {
-        echo '
-        <h1>' . $array->usn . '</h1>
-        <h1>' . $array->student_email . '</h1>
-        <h1>' . $array->student_name . '</h1>
-        <h1>' . $array->branch_name . '</h1>
-        ';
+function usn_query($con)
+{
+    if ($con) {
+        $query = "SELECT `usn`, `student_email`, `student_name`, `branch_name` FROM `student` WHERE `usn`='1BM20CS001'";
+        $res = $con->query($query);
     }
-} else {
-    echo 'Data not found';
+    if ($res) {
+        while ($array = $res->fetch(PDO::FETCH_OBJ)) {
+            return $array;
+        }
+    }
+    return $array;
 }
 ?>
--- profile.php
+++ profile.php
@@ -2,12 +2,14 @@
 
 require_once("configuration/config.php");
 require_once("configuration/auth.php");
+require_once("driver_code/php/retrieval.php");
 
 session_start();
 
-if (!User::isloggedin()) {
-    header("Location: login.php");
-}
+// if (!User::isloggedin()) {
+//     header("Location: login.php");
+// }
+
 ?>
 
 <!DOCTYPE html>
@@ -28,34 +30,68 @@ if (!User::isloggedin()) {
     <?php require('driver_code/html/header.html'); ?>
 
 
-    <!-- student info pallet -->
-    <div id="profile_pallet" name="profile_pallet">
+    <div class="pallets">
+        <!-- student info pallet -->
+        <div id="profile_pallet" name="profile_pallet">
 
-        <!-- Place for image of the student -->
-        <img src="./images/trans_profile_img-removebg-preview.png" alt="PROFILE_PHOTO" id="in_pallet_profile">
+            <!-- // -->
+            <!-- Place for image of the student -->
+            <!-- // -->
+            <!-- <?php
+                    //         if (!profile) {
+                    //             echo '';
+                    //         } else {
+                    //             //show profile
+                    //         }
+                    // 
+                    ?> -->
+            <!-- // -->
+            <!-- Place for name of the student -->
+            <!-- // -->
+            <!-- <div id="name"></div> -->
 
-        <!--Place for name of the student-->
-        <div id="name"></div>
 
+
+            <div class="profile_image_container">
+                <div class="profile_image">
+                    <img src="./images/trans_profile_img-removebg-preview.png" alt="PROFILE_PHOTO" id="in_pallet_profile">
+                </div>
+
+                <!-- profile image edit symbol -->
+                <!-- <div class="profile_image_editor">
+                        <form action="post">
+                            <input type="file" value="change" class="change_photo">
+                        </form>
+                    </div> -->
+
+            </div>
+
+            <div class="name">
+                <?php
+                echo usn_query($con);
+                ?>
+            </div>
+
+        </div>
+
+
+        <!-- results pallete-->
+        <a href="./result.html" class="links_in_pallets">
+            <div id="result" name="result">
+                <h1 class="result_heading">YOUR RESULTS</h1>
+                <img src="./images/results.png" alt="RESULTS" class="results_logo">
+            </div>
+        </a>
+
+
+        <!-- Application form -->
+        <a href="./form1.html" class="links_in_pallets">
+            <div id="appl_form" name="appl_form">
+                <h1>EXAM APPLICATION</h1>
+                <img src="./images/application.png" alt="APPLICATION_FORM" class="application_logo">
+            </div>
+        </a>
     </div>
-
-
-    <!-- results pallete-->
-    <a href="./result.html" style="color: black;">
-        <div id="result" name="result">
-            <h1>YOUR RESULTS</h1>
-            <img src="./images/results.png" alt="RESULTS" class="results_logo">
-        </div>
-    </a>
-
-
-    <!-- Application form -->
-    <a href="./form1.html" style="color: black;">
-        <div id="appl_form" name="appl_form">
-            <h1>EXAM APPLICATION</h1>
-            <img src="./images/application.png" alt="APPLICATION_FORM" class="application_logo">
-        </div>
-    </a>
 
 
     <!--Some bullshit-->
