2fd79e1a3fa1252decc053f778856d422f0b7efb
vinay kulkarni <kulkarnivinay621@gmail.com>
2022-08-09 23:40:43 +0530

    aug_9 updated enrolled table, inserted values into student,course and enrolled table.

--- sql/tables.sql
+++ sql/tables.sql
@@ -48,7 +48,6 @@ CREATE TABLE `website`.`enrolled`
     `usn` VARCHAR(10) NOT NULL ,
     `course_id` VARCHAR(15) NOT NULL ,
     `course_name` VARCHAR(50) NOT NULL ,
-    PRIMARY KEY (`usn`(5)),
     FOREIGN key(`usn`) REFERENCES `student`(`usn`) 
     ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN key(`course_id`) REFERENCES `course`(`course_id`) 
@@ -112,7 +111,59 @@ INSERT INTO `branch`(`branch_name`, `hod`) VALUES ('AIML','Gowrishankar');
 
 
 
--- pending
-
 -- inserting into student table
-INSERT INTO `student`(`usn`, `student_name`, `student_email`, `branch_name`, `password`) VALUES ('1BM18CS001','ADARSH','adarsh.cs18@bmsce.ac.in','CSE','19Nndc*!!knsc');
+INSERT INTO `student`(`usn`, `student_name`, `student_email`, `branch_name`, `password`) VALUES ('1BM18CS001','Vinay','vinay.cs18@bmsce.ac.in','CSE','12345');
+INSERT INTO `student`(`usn`, `student_name`, `student_email`, `branch_name`, `password`) VALUES ('1BM18CS002','Vignesh','vignesh.cs18@bmsce.ac.in','CSE','12345');
+INSERT INTO `student`(`usn`, `student_name`, `student_email`, `branch_name`, `password`) VALUES ('1BM18CS003','Vinayak','vinayak.cs18@bmsce.ac.in','CSE','12345');
+
+
+-- inserting into course table
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19MA3BSSDM','Statistics and Discrete Mathematics','4','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3ESMMC','Microprocessors and Microcontrollers','4','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3PCOOJ','Object Oriented Java Programming','4','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3PCDST','Data Structures','3','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3PCCOA','Computer Organization and Architecture','3','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3PCLOD','Logic Design','4','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3PWPW1','Project Work-1','2','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS3NCNC3','Physical Activity (Sports/ Yoga Etc.)','0','3');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19HS3ICEVS','Environmental Studies','1','3');
+
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19MA4BSLIA','Linear Algebra','4','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4PCTFC','Theoretical Foundations of Computations','4','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4PCDBM','Database Management Systems','4','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4PCADA','Analysis and Design of Algorithms','4','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4PCOPS','Operating Systems','4','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4SRSTI','Seminar Technical / Internship','1','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4PWPW2','Project Work-2','2','4');
+INSERT INTO `course`(`branch_name`, `course_id`, `course_name`, `credit`, `semester`) VALUES ('CSE','19CS4NCNC4','Cultural Activity (Music/Dance etc.)','0','4');
+
+
+-- inserting into enrolled table
+-- 3rd sem
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19MA3BSSDM','Statistics and Discrete Mathematics');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19MA3BSSDM','Statistics and Discrete Mathematics');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19MA3BSSDM','Statistics and Discrete Mathematics');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3ESMMC','Microprocessors and Microcontrollers');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3ESMMC','Microprocessors and Microcontrollers');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3ESMMC','Microprocessors and Microcontrollers');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3PCOOJ','Object Oriented Java Programming');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3PCOOJ','Object Oriented Java Programming');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3PCOOJ','Object Oriented Java Programming');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3PCDST','Data Structures');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3PCDST','Data Structures');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3PCDST','Data Structures');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3PCCOA','Computer Organization and Architecture');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3PCCOA','Computer Organization and Architecture');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3PCCOA','Computer Organization and Architecture');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3PCLOD','Logic Design');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3PCLOD','Logic Design');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3PCLOD','Logic Design');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3PWPW1','Project Work-1');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3PWPW1','Project Work-1');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3PWPW1','Project Work-1');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19CS3NCNC3','Physical Activity (Sports/ Yoga Etc.)');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19CS3NCNC3','Physical Activity (Sports/ Yoga Etc.)');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19CS3NCNC3','Physical Activity (Sports/ Yoga Etc.)');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS001','19HS3ICEVS','Environmental Studies');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS002','19HS3ICEVS','Environmental Studies');
+INSERT INTO `enrolled`(`usn`, `course_id`, `course_name`) VALUES ('1BM20CS003','19HS3ICEVS','Environmental Studies');
